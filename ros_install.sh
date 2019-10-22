cbfb42b3647d1ea67d4a4f218d2dead7b7a7e80d
mgtm <m.g.t.m.prog@gmail.com>
2019-10-22 23:34:16 +0200

    added ros installer script

---
+++ ros_install.sh
@@ -0,0 +1,17 @@
+  GNU nano 2.5.3                                            File: setup.sh                                                                                               
+
+echo 'Adding sources.list ...'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+echo 'Setup keys ..'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+echo 'Updating packages'
+sudo apt-get update
+echo 'Installing ros'
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essentia
+
+
