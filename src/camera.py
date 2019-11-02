8ebabc6ec6586baedb954544a6205ad870129a33
Hassan <hassanelshazly2010@gmail.com>
2019-11-02 16:58:44 +0200

    Add camera node

---
+++ src/camera.py
@@ -0,0 +1,41 @@
+import gi
+
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+
+class Camera():
+    def __init__(self, name, index, host, port):
+        Gst.init(None)
+        self.index = index
+        self.name = name
+        _pipeline = "v4l2src device=/dev/video" + index + " ! image/jpeg,width=1920,height=1080,framerate=30/1 " \
+                  + "! rtpjpegpay ! udpsink host=" + host + " port=" + port
+        self.pipeline = Gst.parse_launch(_pipeline)
+        self.running = False
+
+    def start(self):
+        try:
+            ret = self.pipeline.set_state(Gst.State.PLAYING)
+            if ret == Gst.StateChangeReturn.FAILURE:
+                raise Exception("Error starting the pipeline")
+            self.running=True
+            print(self.name + " of index " + self.index + " started")
+        except :
+            print(self.name + " index " + self.index + " is not Connected")
+            return
+
+    def pause(self):
+        self.pipeline.set_state(Gst.State.PAUSED)
+        self.running = False
+        print(self.name + " of index " + self.index + " paused")
+    
+    def close(self):
+        self.running = False
+        self.pipeline.set_state(Gst.State.NULL)
+        print(self.name + " of index " + self.index + " stopped")
+
+    def is_running(self):
+        return self.running
+
+    def info(self):
+        return self.name, self.index
---
+++ src/camera_node.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import rospy
+import json
+from std_msgs.msg import String
+from camera import Camera    
+
+def camera_callback(data):
+    global camera
+    global servo_pub
+
+    msg = json.load(data)
+    index = int(msg["index"])  
+    action = msg["action"]
+
+    if action == "move":
+        servo_msg = { camera[index].info()[0] : msg["angle"] }
+        servo_pub.publish(servo_msg)
+    elif action == "start":
+        camera[index].start()
+    elif action == "pause":
+        camera[index].pause()
+    elif action == "stop":
+        camera[index].stop()
+    return
+
+if __name__ == "__main__" :
+    rospy.init_node("camera")
+    rospy.Subscriber("QT", String, camera_callback)
+    servo_pub = rospy.Publisher("camera_servo", String, queue_size = 5)     
+
+    host = "192.168.1.4"
+    port1 = 5000
+    port2 = 5001
+    
+    camera = []
+    camera.append(Camera("Main_Cam", 0, host, port1))
+    camera.append(Camera("Back_Cam", 1, host, port2))
+    
+    rospy.spin()
+
