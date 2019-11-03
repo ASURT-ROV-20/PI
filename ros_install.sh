eb3441c162f4be0e358d0cf8bc104fc37b7864da
mgtm <m.g.t.m.prog@gmail.com>
2019-11-03 18:03:07 +0200

    modified ros_install.sh

--- ros_install.sh
+++ ros_install.sh
@@ -1,5 +1,6 @@
   GNU nano 2.5.3                                            File: setup.sh                                                                                               
 
+cd ~
 echo 'Adding sources.list ...'
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo 'Setup keys ..'
@@ -7,11 +8,31 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31
 echo 'Updating packages'
 sudo apt-get update
 echo 'Installing ros'
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full -y
 sudo rosdep init
 rosdep update
+echo "Adding sys vars"
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essentia
+exec bash
+echo "installing python dependences"
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+echo "Creating catkin_ws"
+mkdir catkin_ws/src
+cd catkin_ws
+catkin_make
+echo "configuering catkin_ws"
+cd devel
+path=$(pwd)
+path=$path"/setup.bash"
+echo "source $path" >> ~/.bashrc
+source ~/.bashrc
+exec bash
+cd ../src
+echo "cloning base repo"
+git clone https://github.com/ASURT-ROV-20/PI
+cd PI
+
+
 
 
