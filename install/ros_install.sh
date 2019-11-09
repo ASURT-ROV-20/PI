7653bfcaa9f2ef3c4fae47034eafca5735ac0e78
mgtm <m.g.t.m.prog@gmail.com>
2019-11-09 13:10:43 +0200

    modified ros_install.sh

--- install/ros_install.sh
+++ install/ros_install.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
+info=`tput setaf 6`
+normal=`tput sgr0`
+bold=`tput bold`
+
 cd ~
-echo 'Adding sources.list ...'
+echo ${info}${bold}'Adding sources.list ...'${normal}
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-echo 'Setup keys ..'
+echo ${info}${bold}'Setup keys ..'${normal}
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-echo 'Updating packages'
+echo ${info}${bold}'Updating packages'
 sudo apt-get update
-echo 'Installing ros'
+echo ${info}${bold}'Installing ros'${normal}
 sudo apt-get install -y ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
-echo "Adding sys vars"
+echo ${info}${bold}"Adding sys vars"${normal}
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 exec bash
-echo "installing python dependences"
+echo ${info}${bold}"installing python dependences"${normal}
 sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
 pip3 install pyyaml
 pip3 install rospkg
-echo "Creating catkin_ws"
+echo ${info}${bold}"Creating catkin_ws"${normal}
 mkdir catkin_ws/src
 cd catkin_ws
 catkin_make
-echo "configuering catkin_ws"
+echo ${info}${bold}"configuering catkin_ws"${normal}
 cd devel
 path=$(pwd)
 path=$path"/setup.bash"
@@ -31,7 +35,7 @@ echo "source $path" >> ~/.bashrc
 source ~/.bashrc
 exec bash
 cd ../src
-echo "cloning base repo"
+echo ${info}${bold}"cloning base repo"${normal}
 git clone https://github.com/ASURT-ROV-20/PI
 cd PI
 
