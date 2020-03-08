6a5899cdce76403da750f31891dfec5f7b0e9508
Seif Eldin Mohamed <s.seif26@yahoo.com>
2020-03-08 16:25:39 -0400

    solved NAN issue

--- src/hardware.py
+++ src/hardware.py
@@ -6,8 +6,8 @@ import rospy
 from std_msgs.msg import String
 import time
 import json
-
-devices = {}
+import math
+motors = {}
 cameras = {}
 
 Zero_thruster = 340
@@ -17,68 +17,61 @@ hat = PCA9685()
 hat.set_pwm_freq(50)
 delay = 0.000020
 
-def add_Device(name,channel,zero_value):
-    devices[name] = {'channel':channel , 'zero':zero_value , 'current': zero_value}\
+def add_Motor(name,channel,zero_value):
+    motors[name] = {'channel':channel , 'zero':zero_value , 'current': zero_value}
 def add_Camera(name,channel,zero_value):
     cameras[name] = {'channel':channel , 'zero':zero_value , 'current': zero_value}
 
 def updateMotorPWM(pwms_json):
-    pwms = json.loads(pwms_json.data)
-    for key in pwms.keys():
-        devices[key]['current'] = pwms[key]
-    for key in devices.keys():
-#        hat.set_pwm(devices[key]['channel'],0,int(devices[key]['current'] + 340))
-        if True:
-            print(key, devices[key]['channel'],devices[key]['zero'],devices[key]['current']+devices[key]['zero'])
-            hat.set_pwm(devices[key]['channel'],0,int(devices[key]['current'] + devices[key]['zero']))
-        time.sleep(delay)
+	pwms = json.loads(pwms_json.data)
+	for key in pwms.keys():
+		motors[key]['current'] = pwms[key]
+	for key in motors.keys():
+		current_pwm = motors[key]['current']
+		if not math.isnan(current_pwm):
+		    print(key, motors[key]['channel'],int(current_pwm+motors[key]['zero']))
+		    # hat.set_pwm(motors[key]['channel'],0,int(current_pwm + motors[key]['zero']))
+		time.sleep(delay)
 
 def updateCameraPWM(pwms_json):
-    pwms = json.loads(pwms_json.data)
-    for key in pwms.keys():
-        cameras[key]['current'] = pwms[key]
-    for key in cameras.keys():
-#        hat.set_pwm(devices[key]['channel'],0,int(devices[key]['current'] + 340))
-        if True:
-            print(key, cameras[key]['channel'],cameras[key]['zero'],cameras[key]['current']+cameras[key]['zero'])
-            hat.set_pwm(cameras[key]['channel'],0,int(cameras[key]['current'] + cameras[key]['zero']))
-        time.sleep(delay)
+	pwms = json.loads(pwms_json.data)
+	for key in pwms.keys():
+		cameras[key]['current'] = pwms[key]
+	for key in cameras.keys():
+		current_pwm = cameras[key]['current']
+		if not math.isnan(current_pwm):
+		    print(key, cameras[key]['channel'],int(current_pwm + cameras[key]['zero']))
+		    #hat.set_pwm(cameras[key]['channel'],0,int(current_pwm + cameras[key]['zero']))
+		time.sleep(delay)
 
 def updateSinglePWM(name,current):
-    devices[name]['current'] = current
-    hat.set_pwm(devices[name]['channel'],devices[name]['zero'],devices[name]['current'])
+    motors[name]['current'] = current
+    hat.set_pwm(motors[name]['channel'],motors[name]['zero'],motors[name]['current'])
 
     time.sleep(delay)
 
-#def PID_Control(pwm_z):
-#         pwm = pwm_z.data
-#         if abs ( devices['Vertical_Left']['current'] - pwm ) <= 2 or (devices['Vertical_Right']['current'] - pwm ) <= 2:
-#                 return
-# 
-# 
-#         time.sleep(delay)
-# 
-#         devices['Vertical_Left']['current'] =pwm
-#         devices['Vertical_Right']['current'] =pwm
-#         print("control value:: ",pwm)
+def addHardwareDevices():
+	add_Motor('Left_Front', 14, Zero_thruster)
+	add_Motor('Right_Front', 15, Zero_thruster)
+	add_Motor('Right_Back', 11, Zero_thruster)
+	add_Motor('Left_Back', 10, Zero_thruster)
+	add_Motor('Vertical_Right', 13, Zero_thruster)
+	add_Motor('Vertical_Left', 12, Zero_thruster)
+	add_Camera('cam1',0,Zero_Servo)
+	add_Camera('cam2',1,Zero_Servo)
+	add_Camera('cam3',2,Zero_Servo)
 
 def main():
-    add_Device('Left_Front', 14, Zero_thruster)
-    add_Device('Right_Front', 15, Zero_thruster)
-    add_Device('Right_Back', 11, Zero_thruster)
-    add_Device('Left_Back', 10, Zero_thruster)
-    add_Device('Vertical_Right', 13, Zero_thruster)
-    add_Device('Vertical_Left', 12, Zero_thruster)
-    add_Camera('cam1',2,Zero_Servo)
-    add_Camera('cam2',1,Zero_Servo)
-    add_Camera('cam3',99,Zero_Servo) # todo set correct channel
-
+    addHardwareDevices()
     rospy.init_node('Hardware')
     rospy.Subscriber('Equations',String,updateMotorPWM)
     rospy.Subscriber('Servos',String,updateCameraPWM)
-    #rospy.Subscriber("Control",Float64,PID_Control)
 
     rospy.spin()
 
 
-main()
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
